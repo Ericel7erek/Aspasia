821d411a92f2db5f4eca5614f08f9177fec971bb
mañana <ericalaa@ymail.com>
2024-04-04 13:55:20 +0200

    js

--- jota.js
+++ jota.js
@@ -1,23 +1,23 @@
-// const replaceAt = (string, character, index) => {
-//     return string.substring(0, index) + character + string.substring(index + character.length);
-// }
+const replaceAt = (string, character, index) => {
+    return string.substring(0, index) + character + string.substring(index + character.length);
+}
 
-// const words = ["elon","rex","integral","casa"]
-// let secretWord = words[Math.floor(Math.random()*words.length)]
-// let hiddenWord = secretWord.replace(/./g, "_ ");
-// let isGameAlive = true
-// while (isGameAlive) {
-//     alert(hiddenWord)
-//     let input = prompt("Introduce una letra");
-//     for (let i = 0; i < secretWord.length; i++) {
-//             if (input.toLowerCase()==secretWord[i]) {
-//                 hiddenWord= replaceAt(hiddenWord, input, i*2)
-//             }else if (!hiddenWord.includes("_"))  {
-//                 isGameAlive = false
-//                 alert("Congrats!!!")
-//             }
-//         }
-// }
+const words = ["elon","rex","integral","casa"]
+let secretWord = words[Math.floor(Math.random()*words.length)]
+let hiddenWord = secretWord.replace(/./g, "_ ");
+let isGameAlive = true
+while (isGameAlive) {
+    alert(hiddenWord)
+    let input = prompt("Introduce una letra");
+    for (let i = 0; i < secretWord.length; i++) {
+            if (input.toLowerCase()==secretWord[i]) {
+                hiddenWord= replaceAt(hiddenWord, input, i*2)
+            }else if (!hiddenWord.includes("_"))  {
+                isGameAlive = false
+                alert("Congrats!!!")
+            }
+        }
+}
 
 
 const n=["Eric","Eric","Eric","Eric","Eric"]
@@ -66,12 +66,12 @@ console.log(filter);
 const filter2 = users.filter(user=> user.edad>18)
 console.log(filter2);
 
-const words = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+// const words = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
 
-const frase = words.reduce((acc, word) => `${acc} ${word}`);
-console.log(frase); // a long time ago in a galaxy far far away
+// const frase = words.reduce((acc, word) => `${acc} ${word}`);
+// console.log(frase); // a long time ago in a galaxy far far away
 
-const datos = [1,2,3]
-const calculador = datos.reduce((acc, num)=> acc+num, 0)/datos.length
-console.log(calculador);
+// const datos = [1,2,3]
+// const calculador = datos.reduce((acc, num)=> acc+num, 0)/datos.length
+// console.log(calculador);
 
