00be720492a0e5e31907481bed03d082a409a7d6
mañana <ericalaa@ymail.com>
2024-04-17 14:01:52 +0200

    Promise All

--- React/src/Components/Starwars/Profile.jsx
+++ React/src/Components/Starwars/Profile.jsx
@@ -1,20 +1,50 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-import { getFilms } from '../../api/api'
+import { getFilms, getInfo, getPeople, getSpecies } from '../../api/api'
 import { useParams } from 'react-router-dom'
 
 const Profile = () => {
     const { id } = useParams()
     const [films, setFilms] = useState([])
-    
+    const [info, setInfo] = useState([])
+    const [species, setSpecies] = useState("")
     useEffect(async() => {
-    const p = await getFilms(id);
-    console.log(p)
-    setFilms('ffffffffff',p)
-}, [id])
-console.log(films);
+        // 1. Obtener la información del personaje
+        getInfo(id).then(data => {
+            
+                getFilms(data.films).then(d =>{
+            setFilms(d)
+        })
+            setInfo(data)}
+        )
+        // 2. Obtener las películas del personaje
+    
+        // 3. Pasar las urls de las películas del personaje al método getFilms()
+        getSpecies(id).then(data=> setSpecies(data.name))
+        
+        
+console.log('rrrrrrrrrrrrrrrrrr', p)
+    console.log(11111,p)
+    // setFilms(p.title)
+}, [])
+console.log(22222,films, info, species);
   return (
-    <div></div>
+<div style={{display: 'flex', justifyContent: 'space-around'}}>
+    <div>
+    <h1>Name: {info.name}</h1>
+    <p>Height: {info.height}</p>
+    <p>Mass: {info.mass}</p>
+    <p>Gender: {info.gender}</p>
+    <p>Skin Color: {info.skin_color}</p>
+    <h2>Films</h2>
+    {films.map(film=> <p>{film.title}</p>)}
+    <h2>Species</h2>
+    <p>{species}</p>
+    </div>
+    <div>
+        <img src={'https://starwars-visualguide.com/assets/img/characters/'+ id+'.jpg'} alt="" /> 
+    </div>
+</div>
   )
 }
 
--- React/src/api/api.js
+++ React/src/api/api.js
@@ -3,14 +3,31 @@ import axios from 'axios'
 const instance = axios.create({baseURL: 'https://swapi.dev/api'})
 
 export const getPeople =async (id=1) =>{
-    console.log('111111111',id)
-    const d =  await instance.get(`/people/${id}`);
-    console.log('dddddddddddd',d)
+    const d =  await instance.get(`/people/?page=${id}`);
     return d.data;
 }
 
-export const getFilms = async (id) =>{
-    const d =  await instance.get(`/films/${id}`);
-    console.log('ppppppppp',d)
+
+export const getFilms = async (urls) =>{
+    const data = await Promise.all(urls.map(async (url) => {
+     const r = await axios.get(url);
+     console.log(r)
+     return r.data;
+     
+}))
+
+console.log('ñññññññññññññññññ',data)
+return data;
+}
+export const getFilm = async (id) =>{
+    const d= await instance.get(`/films/${id}/`);
+}
+export const getInfo = async (id) =>{
+    const d= await instance.get(`/people/${id}`);
+    return d.data
+}
+
+export const getSpecies = async (id) =>{
+    const d= await instance.get(`species/${id}/`)
     return d.data
 }
