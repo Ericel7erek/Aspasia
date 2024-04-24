4f06b6015b79c35b8543dec4000851bfdd166fd0
mañana <ericalaa@ymail.com>
2024-04-24 10:22:42 +0200

    tipo

---
+++ tiposcript/src/Router.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Read from './Components/Read'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Read/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
