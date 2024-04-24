a313bd2633b41c061fd977cca28e276de8107b75
mañana <ericalaa@ymail.com>
2024-04-24 12:05:47 +0200

    New Ts

---
+++ tiposcript2.0/src/Firebase/api.ts
@@ -0,0 +1,15 @@
+import { collection, addDoc, db } from "./firebase";
+
+type todo = {
+    userName: string;
+    id: string;
+}
+
+const collectionName = 'items';
+
+// CREATE
+export const createItem = async(obj:todo):Promise<string> => {
+    const colRef = collection(db, collectionName);
+    const data = await addDoc(colRef, obj);
+    return data.id;
+}
