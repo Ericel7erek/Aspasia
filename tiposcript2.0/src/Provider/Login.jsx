18f299f9d353922da0ae5941854a511f1b9ae3fc
mañana <ericalaa@ymail.com>
2024-05-05 12:00:00 +0200

    R

--- tiposcript2.0/src/Provider/Login.jsx
+++ tiposcript2.0/src/Provider/Login.jsx
@@ -1,53 +1,71 @@
-import { useEffect, useState } from 'react';
-import { access, loginWithGoogle, signIn, signUp } from '../Firebase/api';
-import { useUserContext } from './UserProvider';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../Firebase/firebase';
+import { useEffect, useState } from "react";
+import { loginWithGoogle, signIn, signUp } from "../Firebase/api";
+import { useUserContext } from "./UserProvider";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Firebase/firebase";
 
 const Login = () => {
-    const [email, setEmail]= useState('')
-    const [pass, setPass]= useState('')
-    const [user, setUser] = useUserContext();
-    const [created, isCreated] = useState(true)
-    // const [userName, setUserName] = useState()
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [user, setUser] = useUserContext();
+  const [created, isCreated] = useState(true);
+  // const [userName, setUserName] = useState()
 
-    useEffect(() => {
-    onAuthStateChanged(auth, user => {
-          if (user) {
-            console.log('user', user, ' userId:', user.uid);
-            setUser(user);
-        } else {
-            console.log("No user logged");
-            setUser(null);  
-        }
-    })
-  }, [])
-    return (
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("user", user, " userId:", user.uid);
+        setUser(user);
+      } else {
+        console.log("No user logged");
+        setUser(null);
+      }
+    });
+  }, []);
+  return (
+    <>
+      {user ? (
         <>
-        {user ? 
-        <>
-        <input type="email" onChange={(e)=> setEmail(e.target.value)}/> <br />
-        <input type="password" onChange={(e)=> setPass(e.target.value)}/> <br />
-        <button onClick={()=>{
-            signIn(email,pass)
-            isCreated(!created)
-        }}>Sign In</button>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />{" "}
+          <br />
+          <input
+            type="password"
+            onChange={(e) => setPass(e.target.value)}
+          />{" "}
+          <br />
+          <button
+            onClick={() => {
+              signIn(email, pass);
+              isCreated(!created);
+            }}
+          >
+            Sign In
+          </button>
         </>
-        :
+      ) : (
         <>
-        <input type="email" onChange={(e)=> setEmail(e.target.value)}/> <br />
-        <input type="password" onChange={(e)=> setPass(e.target.value)}/> <br />
-        <button onClick={()=>{
-            signUp(email,pass)
-            isCreated(!created)
-            }}>Create User</button>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />{" "}
+          <br />
+          <input
+            type="password"
+            onChange={(e) => setPass(e.target.value)}
+          />{" "}
+          <br />
+          <button
+            onClick={() => {
+              signUp(email, pass);
+              isCreated(!created);
+            }}
+          >
+            Create User
+          </button>
         </>
-        }
-        <br />
-        <button onClick={()=>isCreated(!created)}>Change</button> <br />
-        <button onClick={()=> loginWithGoogle()}>Login with Google</button><br />
-
-        {/* <input type="text" onChange={(e)=> setUserName(e.target.value)} />
+      )}
+      <br />
+      <button onClick={() => isCreated(!created)}>Change</button> <br />
+      <button onClick={() => loginWithGoogle()}>Login with Google</button>
+      <br />
+      {/* <input type="text" onChange={(e)=> setUserName(e.target.value)} />
         <p>{user?.userName}</p>
         <p>{user?.id}</p>
         <button onClick={async ()=>{ 
@@ -59,8 +77,8 @@ const Login = () => {
             }
     } }
         >Acceder</button> */}
-        </>
-    );
-}
+    </>
+  );
+};
 
 export default Login;
